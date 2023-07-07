b16703ee54319a5b5b5f19e826ad7c7c0b4aca3a
Elaine <elainedoud@gmail.com>
2023-07-07 14:54:02 -0400

    Still working on debugging my code to get API data onto application

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
-const url = 'https://api.github.com/search/repositories?q=language:java&sort=stars&order=desc'
+
+const Url = 'https://api.github.com/search/repositories?q=language:java&sort=stars&order=desc';
+const largeContainer = document.querySelector("#large-container");
+
 
 async function api_call(url){
     const response = await fetch(url, {
@@ -7,8 +10,35 @@ async function api_call(url){
             "Content-Type": "application/json"
 },})
     const info = await response.json();
-    console.log(info);
-
+    show(info);
     }
 
-    console.log(api_call(url))
+    window.onload = () => api_call(Url);
+
+
+    
+
+    function show(info){
+        for (let x of info.items){
+            const infoBox = document.createElement('div');
+            //infoBox = document.querySelector("#info-box");
+            const repoFullName = document.createElement('p');
+            //const createdAt = document.createElement('p');
+            
+            repoFullName.textContent = x.full_name;
+            //createdAt.textContent = x.created_at;
+
+            ;
+            //infoBox.append(createdAt);
+            
+        }
+        infoBox.append(repoFullName);
+        largeContainer.append(infoBox);
+    }
+
+
+    
+    //I don't know how to debug my code further since console.log in 
+    // terminal is not playing nice with DOM elements
+
+    // I don't want to return hundreds of items; maybe that is causing issues
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Project
+
+To view the web application, follow the below steps:
+    1. Fork and download the github repository to your computer
+    2. Within the file, type "npm install http-server -g" and enter
+    3. Then type "http-server"
+    4. Go to your browser and type http://localhost:8080/App.html to view the web application
