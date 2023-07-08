a521b18f97af4b5fc912650925d13ebcabae9fc4
Elaine <elainedoud@gmail.com>
2023-07-08 13:51:20 -0400

    Light style changes

--- App.js
+++ App.js
@@ -8,11 +8,23 @@ fetch(Url)
     names+=     `<div id="large-container">
     <div id="info-box">
         <p id="namestyle">${values.full_name}</p>
+        <p id="namestyle">${values.size}</p>
+        <p id="namestyle">${values.created_at}</p>
+        <p id="namestyle">${values.has_wiki}</p>
+        
     </div>
 </div>`  
  })
  document.getElementById("large-container").innerHTML = names;
 });
+
+//Next Steps:
+// Display more information
+// Fix variable names and add comments
+// Adjust CSS to make it look better
+// Try to make an adjustment so GET 404 error does not appear in 
+// console window
+
 
 // May need to use Command-Shift-R to force refresh the page
 // data.items[0].full_name will display the full name when
