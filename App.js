1f27e77c530f101b74c05d805dd2ae53385c8640
Elaine <elainedoud@gmail.com>
2023-07-08 13:34:36 -0400

    Fetch request now working & info is populating to page

--- App.js
+++ App.js
@@ -1,63 +1,19 @@
-
 const Url = 'https://api.github.com/search/repositories?q=language:java&sort=stars&order=desc';
-const largeContainer = document.querySelector("#large-container");
-const infoBox = document.querySelector("#info-box");
-let count;
 
-//window.onload = 
-//function pullAPI(url) {
 fetch(Url)
-.then(response => response.json())
-.then(data =>{
-    for (let key in data){
-        if (data.hasOwnProperty(key) && key === "items"){
-            const name = document.createElement('p');
-            name.textContent = data[key].full_name;
-            infoBox.appendChild(name); 
-            count += 1;
-        }
-        if (count >= 10){
-            break;
-        }
-    }}
-    )
+.then(res => res.json())
+.then(data => {
+    let names = "";
+ data.items.map((values)=>{
+    names+=     `<div id="large-container">
+    <div id="info-box">
+        <p id="namestyle">${values.full_name}</p>
+    </div>
+</div>`  
+ })
+ document.getElementById("large-container").innerHTML = names;
+});
 
-    largeContainer.appendChild(infoBox);
-    
-//}
-
-//window.onload(Url);
-
-//Now getting a GET Error and Syntax Error
-
-//async function api_call(url){
-    //const response = await fetch(url, {
-       // method: 'GET',
-       // headers: {
-          //  "Content-Type": "application/json"
-//},})
-   // const info = await response.json();
-   // show(info);
-   // }
-
-  //  window.onload = () => api_call(Url);
-
-
-   // function show(info){
-       // for (let x of info.items){
-          //  const infoBox = document.createElement('div');
-            //infoBox = document.querySelector("#info-box");
-         //   const repoFullName = document.createElement('p');
-            //const createdAt = document.createElement('p');
-            
-        //    repoFullName.textContent = x.full_name;
-            //createdAt.textContent = x.created_at;
-
-        //    ;
-            //infoBox.append(createdAt);
-            
-     //   }
-     //   infoBox.append(repoFullName);
-     //   largeContainer.append(infoBox);
-  //  }
-  //
+// May need to use Command-Shift-R to force refresh the page
+// data.items[0].full_name will display the full name when
+// using console.log
